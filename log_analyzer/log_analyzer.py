bd83e96a8e8e160c06731e5626fb15999bd66c5c
anton.rijov <anton.rijov@gmail.com>
2020-06-15 22:30:38 +0300

    first working script

---
+++ log_analyzer/log_analyzer.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import fnmatch
+import gzip
+import json
+import logging
+import os
+import re
+from operator import itemgetter
+from string import Template
+
+config = {
+    "REPORT_SIZE": 1,
+    # "REPORT_SIZE": 1000,
+    "REPORT_DIR": "./reports",
+    "LOG_DIR": "./log",
+    "LOG_FILE_MASK": "*nginx-access-ui.log-????????*"
+}
+
+
+def set_logger():
+    handlers = []
+    log_file = "log_analyzer.log"
+    handlers.append(logging.FileHandler(log_file))
+    handlers.append(logging.StreamHandler())
+    logging_level = logging.INFO
+    logging.basicConfig(
+        level=logging_level,
+        format="[%(asctime)s] %(levelname).1s %(message)s",
+        datefmt="%Y.%m.%d %H:%M:%S",
+        handlers=handlers
+    )
+    return logging.getLogger('tcpserver')
+
+
+def get_date_from_postfix(file_name):
+    return re.search(r"\d{8}", file_name)[0]
+
+
+def get_file_ext(file_name):
+    ext = re.split(r"\.", file_name)[-1]
+    if ext.startswith("log-"):
+        ext = ""
+    return ext
+
+
+def gen_find(file_part, top):
+    for path, dir_list, file_list in os.walk(top):
+        for name in filter(lambda x: get_file_ext(x) in ["", "gz"], fnmatch.filter(file_list, file_part)):
+            yield os.path.join(path, name)
+
+
+def get_recent_filename(filenames):
+    return max(filenames, key=get_date_from_postfix)
+
+
+def open_log_file(filename):
+    if filename.endswith(".gz"):
+        return gzip.open(filename)
+    else:
+        return open(filename)
+
+
+def get_url(line):
+    search_result = re.search(r'("GET |"POST )(.*?)[" ]', line)
+    if search_result:
+        return search_result.group(2)[0:128]
+    else:
+        return f'wrong url on line: {line}'
+
+
+def get_duration(line):
+    return float(re.split(r" ", line)[-1])
+
+
+def gen_parser(lines):
+    for line in lines:
+        yield get_url(line), get_duration(line)
+
+
+def median(lst):
+    half = len(lst) // 2
+    lst.sort()
+    if not len(lst) % 2:
+        return (lst[half - 1] + lst[half]) / 2.0
+    return lst[half]
+
+
+def collect_statistics(filename):
+    def align(d):
+        return round(d, 3)
+
+    urls = dict()
+    try:
+        with open_log_file(filename) as f:
+            lines = gen_parser(f.readlines())
+            for line in lines:
+                url = line[0]
+                duration = line[1]
+                if url in urls:
+                    urls[url].append(duration)
+                else:
+                    urls[url] = [duration]
+    except OSError:
+        logger.error(f"os error ({repr(exc)}) when working with source file ({filename}).")
+        return None
+    total_count = 0
+    total_time = 0.000
+    result_list = list()
+    for url, durations in urls.items():
+        result_row = dict()
+        result_row["url"] = url
+        result_row["count"] = count = len(durations)
+        result_row["time_sum"] = time_sum = align(sum(durations))
+        result_row["time_avg"] = 0 if count == 0 else align(time_sum / count)
+        result_row["time_max"] = max(durations)
+        result_row["time_med"] = align(median(durations))
+        total_time += time_sum
+        total_count += count
+        result_list.append(result_row)
+    for row in result_list:
+        row["time_perc"] = align(row["time_sum"] * 100 / total_time)
+        row["count_perc"] = align(row["count"] * 100 / total_count)
+    return result_list
+
+
+def squeeze_out(lst):
+    lst.sort(reverse=True, key=itemgetter("time_sum"))
+    lst = lst[:config["REPORT_SIZE"]]
+    return lst
+
+
+def make_report():
+    filenames = gen_find(config["LOG_FILE_MASK"], config["LOG_DIR"])
+    recent = get_recent_filename(filenames)
+    if os.path.isfile(recent):
+        logger.info(f"recent log file: {recent}")
+    else:
+        logger.info(f"nothing to analyze.")
+        return
+    date = get_date_from_postfix(recent)
+    report_file_name = os.path.join(config["REPORT_DIR"], "report-" + date[0:4] + '.' + date[4:6] + '.' + date[6:8] + '.html')
+    if os.path.isfile(report_file_name):
+        logger.info(f"report {report_file_name} already exists.")
+        return
+    result_list = collect_statistics(recent)
+    if not result_list:
+        logger.error("empty statistic data")
+        return
+    json_array = json.dumps(squeeze_out(result_list))
+    try:
+        with open("report.html", "r") as template_file:
+            template_string = template_file.read()
+            result_string = Template(template_string).safe_substitute(table_json=json_array)
+            with open(report_file_name, "w") as report_file:
+                report_file.write(result_string)
+    except OSError as e:
+        logger.error(f"os error ({repr(e)}) when working with template file.")
+
+
+def main():
+    logger.info(os.path.basename(__file__) + " started.")
+    make_report()
+    logger.info(os.path.basename(__file__) + " finished.")
+
+
+logger = set_logger()
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as exc:
+        logger.exception(f"exception in main() {repr(exc)}")
+        exit(1)
